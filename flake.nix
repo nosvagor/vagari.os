e6385c3f3aa2962fa09bfb6fd7e7ab99c57f9107
cullyn <cullyn@trendcapital.com>
2025-03-30 20:12:25 -0700

    fix

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
   # ╔═╗╦ ╦╔╦╗╔═╗╦ ╦╔╦╗╔═╗
   # ║ ║║ ║ ║ ╠═╝║ ║ ║ ╚═╗                                       abbot | costello
   # ╚═╝╚═╝ ╩ ╩  ╚═╝ ╩ ╚═╝ ------------------------------------------------------
-  outputs = { self, nixpkgs, home-manager, sops-nix, inputs, ... }:
+  outputs = { self, nixpkgs, home-manager, sops-nix, ... }:
 
     # mkSystem {machine} -> system config
     let
@@ -43,14 +43,14 @@
       primaryUser = "nosvagor"; 
       mkSystem = machineName: pkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit inputs primaryUser machineName; };
+        specialArgs = { inherit primaryUser machineName; };
         modules = [
           ./machines/${machineName}/configuration.nix
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.${primaryUser} = import ./home/${machineName}.nix;
-            home-manager.extraSpecialArgs = { inherit inputs primaryUser machineName; };
+            home-manager.extraSpecialArgs = { inherit primaryUser machineName; };
             
             # Configure Home Manager itself
             home = {
